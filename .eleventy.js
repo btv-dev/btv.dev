027d66f0825d5fe85dcd5a5e6fbd76d2463c7fde
James <jamestedesco802@gmail.com>
2020-09-08 08:35:23 -0400

    Init 11ty & refactor header

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("assets");
+
+    return {
+
+        dir: {
+            // ⚠️ These values are both relative to your input directory.
+            includes: "assets",
+            layouts: "_layouts"
+        }
+
+    }
+};
