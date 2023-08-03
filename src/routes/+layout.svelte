ca58fe36f1d4b8b6eff49cf80861978636a24235
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-03 22:04:17 +0200

    [#18] Remove unused Google Fonts & stylesheet calls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,6 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>btv.dev</title>
-
-		<link
-			href="https://fonts.googleapis.com/css2?family=Modak&display=swap?text=btv.dev"
-			rel="stylesheet"
-		/>
-		<link rel="stylesheet" href="assets/styles.css" />
 	</head>
 	<body>
 		<slot />
