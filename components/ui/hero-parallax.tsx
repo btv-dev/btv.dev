5b7b8debeffdff14e57528292646315ed9b52327
James Mitofsky <jamesmitofsky@gmail.com>
2025-01-21 21:14:01 +0100

    Remove link element from client examples sections (as it led nowhere)

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -175,10 +175,10 @@ export const ProductCard = ({
       key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"
     >
-      <Link
+      {/* <Link
         href={product.link}
         className="block group-hover/product:shadow-2xl "
-      >
+      > */}
         <Image
           src={product.thumbnail}
           height="600"
@@ -186,7 +186,7 @@ export const ProductCard = ({
           className="object-cover object-center absolute h-full w-full inset-0"
           alt={product.title}
         />
-      </Link>
+      {/* </Link> */}
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
