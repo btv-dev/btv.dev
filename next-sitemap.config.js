8166e5900d8429e2e7b29ba083c08589010c3276
James Mitofsky <jamesmitofsky@gmail.com>
2025-01-27 10:22:46 +0100

    Tidy up the sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,17 +2,7 @@
 module.exports = {
   siteUrl: 'https://www.btv.dev',
   generateRobotsTxt: true,
-  generateIndexSitemap: true,
+  generateIndexSitemap: false,
   changefreq: 'weekly',
-  priority: 0.7,
-  exclude: ['/404', '/500'], // Add any paths you want to exclude
-  robotsTxtOptions: {
-    additionalSitemaps: [],
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  },
+  priority: 0.7
 }
