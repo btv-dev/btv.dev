947cb773631fcc0e41f5d3383b3d51d4a10168d7
James Mitofsky <jamesmitofsky@gmail.com>
2025-01-31 14:44:14 +0100

    fixup

--- components/ui/animated-icons/next-gen/share.tsx
+++ components/ui/animated-icons/next-gen/share.tsx
@@ -40,7 +40,7 @@ const UploadIcon = ({ className = "", size = 28, isAnimate = false }: AnimatedIc
         xmlns="http://www.w3.org/2000/svg"
         width={size}
         height={size}
-        viewBox="0 0 24 24"
+        viewBox="-2 -2 28 28"
         fill="none"
         stroke="currentColor"
         strokeWidth="2"
